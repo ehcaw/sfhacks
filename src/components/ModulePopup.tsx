63e86edd666f3fb10244d337d08ecf03f574420e
ehcaw <ryannguyenc@gmail.com>
2024-04-07 04:21:58 -0700

    fixed the audio visualizer logic :D

--- src/components/ModulePopup.tsx
+++ src/components/ModulePopup.tsx
@@ -1,6 +1,6 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { Message as MessageType } from "@/ion/types";
-import {Chatbox} from "@/ion/Chatbox";
+import { Chatbox } from "@/ion/Chatbox";
 import Recorder from "./Recorder"; // Make sure the path is correct and adjust according to your project structure
 
 export const ChatCard: React.FC<{ onClose: () => void }> = ({ onClose }) => {
@@ -10,20 +10,23 @@ export const ChatCard: React.FC<{ onClose: () => void }> = ({ onClose }) => {
 
   const uploadAudio = async (blob: Blob) => {
     const formData = new FormData();
-    formData.append('audio', blob, 'audio.webm'); // Assuming 'audio' is the expected field name on your server
+    formData.append("audio", blob, "audio.webm"); // Assuming 'audio' is the expected field name on your server
     try {
-      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/whisper`, {
-        method: 'POST',
-        body: formData,
-      });
-  
-      if (!response.ok) throw new Error('Network response was not ok');
-  
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/whisper`,
+        {
+          method: "POST",
+          body: formData,
+        }
+      );
+
+      if (!response.ok) throw new Error("Network response was not ok");
+
       const result = await response.json();
-      console.log('Server response:', result);
+      console.log("Server response:", result);
       // Process server response here. E.g., display the transcription or send it for further processing.
     } catch (error) {
-      console.error('Error uploading file:', error);
+      console.error("Error uploading file:", error);
     }
   };
 
@@ -50,21 +53,15 @@ export const ChatCard: React.FC<{ onClose: () => void }> = ({ onClose }) => {
           &times;
         </button>
         {/* Chatbox component might display messages */}
-        <Chatbox messages={[]} /> {/* Assuming Chatbox expects a messages prop */}
-
+        <Chatbox messages={[]} />{" "}
+        {/* Assuming Chatbox expects a messages prop */}
         {/* Recorder component with the ability to start/stop recording and upload audio */}
         <Recorder
           uploadAudio={uploadAudio}
           isRecording={isRecording}
+          toggleRecording={toggleRecording}
         />
-
         {/* Button to toggle recording */}
-        <button
-          className="mt-4 self-center btn"
-          onClick={toggleRecording}
-        >
-          {isRecording ? "Stop Recording" : "Start Recording"}
-        </button>
       </div>
     </div>
   );
--- src/components/Recorder.tsx
+++ src/components/Recorder.tsx
@@ -3,26 +3,43 @@ import { useEffect, useRef, useState } from "react";
 import activeAssistantIcon from "@/img/active.gif";
 import notActiveAssistantIcon from "@/img/notactive.png";
 import React from "react";
+import { start } from "repl";
 
 const mimeType = "audio/webm";
 
 interface RecorderProps {
   uploadAudio: (blob: Blob) => Promise<void>; // Adjusted to return Promise<void> to match your async functio
   isRecording: boolean;
+  toggleRecording: () => void;
 }
 
-function Recorder({ uploadAudio, isRecording }: RecorderProps) {
+const Recorder = ({
+  uploadAudio,
+  isRecording,
+  toggleRecording,
+}: RecorderProps) => {
   const mediaRecorder = useRef<MediaRecorder | null>(null);
   const [pending, setPending] = useState<boolean>(false);
   const [permission, setPermission] = useState(false);
   const [stream, setStream] = useState<MediaStream | null>(null);
-  const [recordingStatus, setRecordingStatus] = useState("inactive");
+  const [recordingStatus, setRecordingStatus] = useState({
+    statisString: "inactive",
+  });
   const [audioChunks, setAudioChunks] = useState<Blob[]>([]);
   const [audio, setAudio] = useState<string | null>(null);
 
   useEffect(() => {
     getMicrophonePermission();
   }, []);
+
+  const combinedFunctions = (mode: string) => {
+    if (mode == "recording") {
+      stopRecording();
+    } else {
+      startRecording();
+    }
+    toggleRecording();
+  };
 
   const getMicrophonePermission = async () => {
     if ("MediaRecorder" in window) {
@@ -46,7 +63,10 @@ function Recorder({ uploadAudio, isRecording }: RecorderProps) {
 
     if (pending) return;
 
-    setRecordingStatus("recording");
+    setRecordingStatus((prevState) => ({
+      ...prevState,
+      statisString: "recording",
+    }));
     //create new Media recorder instance using the stream
     const media = new MediaRecorder(stream, { mimeType: mimeType });
     //set the MediaRecorder instance to the mediaRecorder ref
@@ -67,13 +87,16 @@ function Recorder({ uploadAudio, isRecording }: RecorderProps) {
 
     if (pending) return;
 
-    setRecordingStatus("inactive");
+    setRecordingStatus((prevState) => ({
+      ...prevState,
+      statisString: "inactive",
+    }));
     //stops the recording instance
     mediaRecorder.current.stop();
     mediaRecorder.current.onstop = () => {
       //creates a blob file from the audiochunks data
       const audioBlob = new Blob(audioChunks, { type: mimeType });
-      //creates a playable URL from the blob file.
+      //creates a playable URL from the blob   file.
       const audioUrl = URL.createObjectURL(audioBlob);
       setAudio(audioUrl);
       uploadAudio(audioBlob);
@@ -83,52 +106,139 @@ function Recorder({ uploadAudio, isRecording }: RecorderProps) {
 
   return (
     <div className="flex items-center justify-center">
-      {!permission ? (
-        <button onClick={getMicrophonePermission} type="button">
-          Get Microphone
-        </button>
-      ) : null}
-
-      {pending && (
-        <Image
-          src={activeAssistantIcon}
-          alt="Recording"
-          width={350}
-          height={350}
-          onClick={stopRecording}
-          priority={true}
-          className="assistant grayscale"
-        />
-      )}
-
-      {permission && recordingStatus === "inactive" && !pending ? (
-        <Image
-          src={notActiveAssistantIcon}
-          alt="Not Recording"
-          width={350}
-          height={350}
-          onClick={startRecording}
-          priority={true}
-          className="assistant cursor-pointer hover:scale-110 duration-150 transition-all ease-in-out"
-        />
-      ) : null}
-      {recordingStatus === "recording" ? (
-        <Image
-          src={activeAssistantIcon}
-          alt="Recording"
-          width={350}
-          height={350}
-          onClick={stopRecording}
-          priority={true}
-          className="assistant cursor-pointer hover:scale-110 duration-150 transition-all ease-in-out"
-        />
-      ) : null}
+      <AudioVisualizer audioStream={stream} isRecording={isRecording} />
+      <div className="flex items-center justify-center">
+        {!permission ? (
+          <button onClick={getMicrophonePermission} type="button">
+            Get Microphone
+          </button>
+        ) : null}
+        {permission &&
+        recordingStatus.statisString === "inactive" &&
+        !pending ? (
+          <Image
+            src={notActiveAssistantIcon}
+            alt="Not Recording"
+            width={350}
+            height={350}
+            onClick={() => {
+              combinedFunctions(recordingStatus.statisString);
+            }}
+            priority={true}
+            className="assistant cursor-pointer hover:scale-110 duration-150 transition-all ease-in-out"
+          />
+        ) : null}
+        {recordingStatus.statisString === "recording" ? (
+          <Image
+            src={activeAssistantIcon}
+            alt="Recording"
+            width={350}
+            height={350}
+            onClick={() => {
+              combinedFunctions(recordingStatus.statisString);
+            }}
+            priority={true}
+            className="assistant cursor-pointer hover:scale-110 duration-150 transition-all ease-in-out"
+          />
+        ) : null}
+      </div>
     </div>
   );
-}
+};
 
 export default Recorder;
 
 function setAudioStream(arg0: null) {
   throw new Error("Function not implemented.");
 }
+
+interface AudioVisualizerProps {
+  audioStream: MediaStream | null;
+  isRecording: boolean;
+}
+
+export const AudioVisualizer: React.FC<AudioVisualizerProps> = ({
+  audioStream,
+  isRecording,
+}) => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const audioCtxRef = useRef<AudioContext | null>(null);
+  const [isVisualizing, setIsVisualizing] = useState(false);
+  const minBarHeight = 2;
+
+  useEffect(() => {
+    setIsVisualizing(isRecording);
+  }, [isRecording]);
+
+  useEffect(() => {
+    if (!audioStream || !canvasRef.current) return;
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+    if (!ctx) return;
+    if (!audioCtxRef.current) {
+      try {
+        audioCtxRef.current = new (window.AudioContext ||
+          window.AudioContext)();
+      } catch (e) {
+        console.error("Web Audio API is not supported in this browser", e);
+        return;
+      }
+    }
+    const audioCtx = audioCtxRef.current;
+    const analyser = audioCtx.createAnalyser();
+
+    const source = audioCtx.createMediaStreamSource(audioStream);
+    source.connect(analyser);
+    analyser.fftSize = 256;
+
+    const bufferLength = analyser.frequencyBinCount;
+    const dataArray = new Uint8Array(bufferLength);
+    let animationFrameId: number;
+
+    const drawBars = () => {
+      animationFrameId = requestAnimationFrame(drawBars);
+
+      analyser.getByteFrequencyData(dataArray);
+
+      ctx.fillStyle = "#FFFFFF";
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+      const barWidth = (canvas.width / bufferLength) * 1.5;
+      let x = 0;
+
+      for (let i = 0; i < bufferLength; i++) {
+        let barHeight = minBarHeight;
+        if (isVisualizing) {
+          barHeight = Math.max(
+            minBarHeight,
+            (dataArray[i] / 128.0) * (canvas.height / 2)
+          );
+        }
+
+        ctx.fillStyle = `rgba(35, 43, 43)`;
+
+        ctx.fillRect(
+          x,
+          canvas.height / 2 - barHeight / 2,
+          barWidth - 1,
+          barHeight
+        );
+
+        x += barWidth * 2.5;
+      }
+    };
+
+    drawBars();
+
+    return () => {
+      if (audioCtxRef.current) {
+        source.disconnect();
+        analyser.disconnect();
+        audioCtxRef.current.close();
+        audioCtxRef.current = null;
+        cancelAnimationFrame(animationFrameId);
+      }
+    };
+  }, [audioStream, isVisualizing]);
+  return <canvas ref={canvasRef} className="w-800 h-100" />;
+};
